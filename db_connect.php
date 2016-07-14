8e1ea3a083b3a501274350248d040020f28324f5
sailakshmiv <lakshmi.saiv99@gmail.com>
2016-07-14 16:21:57 +0530

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,6 +1,6 @@
 <?
 $username="root";
-$password="signature";
+$password="trov";
 $database="contacts";
 mysql_connect(localhost,$username,$password);
 @mysql_select_db($database) or die ("unable to select database");
